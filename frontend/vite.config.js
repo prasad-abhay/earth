92a9b8198807898ddaa032814f6400e1593ead56
itsakashprasad <prasadakash.active@gmail.com>
2025-08-03 23:57:31 +0530

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,31 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+
+// export default defineConfig({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   build: {
+//     outDir: "dist/spa",
+//   },
+//   base: "/Earth-Data-Management/",
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./client"),
+//       "@shared": path.resolve(__dirname, "./shared"),
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  build: {
-    outDir: "dist/spa",
-  },
-  // base: "/http://localhost:3000/",
+  base: "/Earth-Data-Management/", // ✅ keep as is
   plugins: [react()],
   resolve: {
     alias: {
