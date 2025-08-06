dd4e61b07fb25bc5c56a199be07b52ae923dff54
itsakashprasad <prasadakash.active@gmail.com>
2025-08-06 14:28:41 +0530

    msg6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-
-// export default defineConfig({
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   build: {
-//     outDir: "dist/spa",
-//   },
-//   base: "/Earth-Data-Management/",
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./client"),
-//       "@shared": path.resolve(__dirname, "./shared"),
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
